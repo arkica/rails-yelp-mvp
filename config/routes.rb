aa84c5df2fe53f7aaa6eae7910cbec29b6cb6134
Armen Kaltak <arkouo@gmail.com>
2021-05-19 15:05:12 +0200

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #get 'restaurants', to: 'restaurants#index'
+  #get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  #get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  #post 'restaurants', to: 'restaurants#create', as: :create do
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
